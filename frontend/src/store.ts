9cb3b533bd133da458cb8e493964dff0f4413e1c
David Cogbill <d_cogbill@yahoo.com>
2023-07-10 17:48:39 -0400

    [potential logo and front end 'template']

---
+++ frontend/src/store.ts
@@ -0,0 +1,14 @@
+export const state:any = {
+    JWT:"",
+
+    //this is an example of how you might store a User object (after parsing the JWT)
+    user: {
+        userId:0,
+        username:"",
+        firstName:"",
+        lastName:""
+    },
+
+    //maybe you want to store an Array of all the user accounts
+    accounts:[]
+}
