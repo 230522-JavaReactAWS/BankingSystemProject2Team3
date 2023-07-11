c8f02387ad6cf4bca1949fb391a1804d8f8de186
imkewlhuh <rafael.lopez0827@gmail.com>
2023-07-11 16:42:41 -0400

    router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './css/App.css';
 import Dashboard from './pages/dashboard';
+import Accounts from "./pages/accounts";
 import reportWebVitals from './reportWebVitals';
+import Transactions from './pages/transactions';
+import ApplyForLoan from './pages/applyForLoan';
+
+//Creating Router instance
+const router = createBrowserRouter([
+    {
+        path: "/dashboard",
+        element: <Dashboard />,
+        children: [
+            {
+                path: "/dashboard/accounts",
+                element: <Accounts />
+            },
+            {
+                path: "/dashboard/transactions",
+                element: <Transactions />
+            },
+            {
+                path: "/dashboard/apply",
+                element: <ApplyForLoan />
+            },
+        ]
+    },
+    {
+        path: "/auth",
+        element: <div>register</div>
+    }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Dashboard />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ frontend/src/pages/transactions.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {
+    Container, Accordion, AccordionSummary,
+    AccordionDetails, Divider, Table,
+    TableHead, TableBody, TableRow,
+    TableCell
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+
+export default function Transactions() {
+
+    const transactionsData = [
+        {
+            date: "7/11/2023",
+            description: "Payment for groceries",
+            type: "withdrawal",
+            amount: "$79.99",
+            from: "Rafael",
+            to: "Grocery store"
+        },
+        {
+            date: "7/11/2023",
+            description: "Shopping at the mall",
+            type: "withdrawal",
+            amount: "$189.47",
+            from: "Rafael",
+            to: "Mall"
+        },
+        {
+            date: "7/11/2023",
+            description: "Paycheck deposit",
+            type: "deposit",
+            amount: "$679.99",
+            from: "Work",
+            to: "Rafael"
+        }
+    ]
+
+    return (
+        <Container sx={{ padding: "2em 1em" }}>
+            <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <h2>View Transactions</h2>
+                </AccordionSummary>
+                <Divider />
+                <AccordionDetails>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Date</TableCell>
+                                <TableCell>Description</TableCell>
+                                <TableCell>Type</TableCell>
+                                <TableCell>Amount</TableCell>
+                                <TableCell>From</TableCell>
+                                <TableCell>To</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                transactionsData.map((transaction, i) => (
+                                    <TableRow key={i}>
+                                        <TableCell>{transaction.date}</TableCell>
+                                <TableCell>{transaction.description}</TableCell>
+                                <TableCell>{transaction.type}</TableCell>
+                                <TableCell>{transaction.amount}</TableCell>
+                                <TableCell>{transaction.from}</TableCell>
+                                <TableCell>{transaction.to}</TableCell>
+                                    </TableRow>
+                                ))
+                            }
+                        </TableBody>
+                    </Table>
+                </AccordionDetails>
+            </Accordion>
+        </Container>
+    )
+}
