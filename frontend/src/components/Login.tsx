e1015b1806235887c27d1e1a3850637a2b9066b4
imkewlhuh <rafael.lopez0827@gmail.com>
2023-07-12 17:03:30 -0400

    loan form and table created

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import "./Login.css"
+import "../css/Login.css"
 import axios from 'axios'
 
 export const Login: React.FC<any> = () => {
